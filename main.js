451fd546c51b893711b338b28b779e85f19fc3e3
sonia <soniayjw@gmail.com>
2023-08-13 08:58:34 +0800

    update js for highligh page

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import './assets/scss/all.scss';
 
 function highlightNavItem(){
-    
-    var index = getNavItemIndex(window.location.pathname);
-    
-    var highlight = document.querySelector(`body > nav > a:nth-child(${index})`);
+    let index = getNavItemIndex();
+    let highlight = document.querySelector(`body > nav > a:nth-child(${index})`);
     highlight.classList.remove("text-dark");
     highlight.classList.add("active", "link-success");
     
 }
 
-function getNavItemIndex(pathname){
-    if(pathname === "/week5/pages/admin.html"){
+function getNavItemIndex(){
+    
+    let pathname = window.location.pathname.split("/").pop();
+    if(pathname === "admin.html"){
         return 6;
     }
 
