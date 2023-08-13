28b7162f5d4d15b0760933c4996efc69180b014d
sonia <soniayjw@gmail.com>
2023-08-13 08:38:01 +0800

    add assignment page

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+function highlightNavItem(){
+    
+    var index = getNavItemIndex(window.location.pathname);
+    
+    var highlight = document.querySelector(`body > nav > a:nth-child(${index})`);
+    highlight.classList.remove("text-dark");
+    highlight.classList.add("active", "link-success");
+    
+}
+
+function getNavItemIndex(pathname){
+    if(pathname === "/week5/pages/admin.html"){
+        return 6;
+    }
+
+    return 4; //default
+}
+
+
+
+
+highlightNavItem();
